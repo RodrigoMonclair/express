aac53a2a7ab7ffd467125db4f4282e12f2e67482
Rodrigo Monclair <rmonclair@gmail.com>
2022-11-28 17:27:47 -0300

    falta o put

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const dadosProcesso = [
+  {
+    id: "89a9bf5c-bcd7-44d3-94f3-144b1fffddde",
+    documentName: "Relação de Remunerações de Contribuição",
+    status: "Em elaboração",
+    details:
+      "Remunerações que incidiram contribuição, no período de 1997 a 2016",
+    dateInit: "28/11/2022",
+    comments: "10 dias úteis para conclusão",
+    datEnd: "",
+    setor: "NUPAC",
+  },
+];
+
+app.get("/all", (req, res) => {
+  return res.status(200).json(dadosProcesso);
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  const form = req.body;
+
+  dadosProcesso.push(form);
+  return res.status(201).json(dadosProcesso);
+});
+
+app.put("/edit/:id", (req, res) => {
+  console.log(req.params);
+    const {id} = req.params;
+    
+  return res.status(201).json(dadosProcesso);
+});
+
+app.delete("/delete/:id", (req, res) => {
+  console.log(req.params); // req. params-> é um objeto e por isso pode ser desconstruído
+  const { id } = req.params;
+
+  const deleteByID = dadosProcesso.find((user) => user.id === id);
+  const index = dadosProcesso.indexOf(deleteByID);
+
+  dadosProcesso.splice(index, 1);
+
+  return res.status(200).json(dadosProcesso);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`run on port http://localhost:${process.env.PORT}`);
+});
